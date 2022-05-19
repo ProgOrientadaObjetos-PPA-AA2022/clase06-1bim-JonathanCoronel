cdbfeaaed82314f3114d217916c2347afd37253f
JonathanCoronel <jacoronel10@utpl.edu.ec>
2022-05-19 16:50:25 -0500

    segundo commit

--- EjemploCiclosRepetitivos/ejemplo-ciclo-while-vendedor/src/paquete02/Vendedor.java
+++ EjemploCiclosRepetitivos/ejemplo-ciclo-while-vendedor/src/paquete02/Vendedor.java
@@ -70,4 +70,19 @@ public class Vendedor {
     public double obtenerComisionPorAuto(){
         return comisionPorAuto;
     }
+    
+    @Override
+    public String toString() {
+        String g = String.format("Datos de Vendedor\n"
+                    + "Nombre: %s\n"
+                    + "Edad: %s\n"
+                    + "Salario mínimo: %.2f\n"
+                    + "Número de autos: %d\n"
+                    + "Pago mensual: %.2f\n\n",
+                    obtenerNombres(), obtenerEdad(),
+                    obtenerSalarioMinimo(), obtenerNumeroAutos(),
+                    obtenerPagoMensual());
+        return g;
+    }
+
 }
